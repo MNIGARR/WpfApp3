e485281f27ec0b927dde51c31179eb827d19f7f5
Musta_cz12 <Musta_cz12@STHQ0124-02>
2023-03-06 21:32:28 +0400

    Add project files.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        public ObservableCollection<Product> Products { get; set; } = new ObservableCollection<Product>
+        {
+
+            new Product
+            {
+                 Name = "corek",
+                 Price = 0.80,
+            },
+            new Product
+            {
+                Name = "sprite",
+                Price = 0.80,
+            }
+        };
+    }
+}
